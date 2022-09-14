79f35d6cfe3bc6ab6db3bf583bfaea4156ce8006
Juan Bazan <jcbazanochoa@gmail.com>
2022-09-13 22:14:45 -0500

    paroject with class components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
-import Testimony from './components/testimony';
+import React, { Component } from 'react';
+import Testimony from './components/Testimony';
 import { testimonies } from './data/testimonies';
-
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <div className='container-main'>
-        <h1>Here is what our alumni say about freeCodeCamp:</h1>
-        {
-          testimonies.map(element => (
-            <Testimony 
-              name={element.name}
-              country={element.country}
-              image={element.image}
-              role={element.role}
-              company={element.company}
-              testimony={element.testimony} />
-              ))
-        }
+export class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className='container-main'>
+          <h1>Here is what our alumni say about freeCodeCamp:</h1>
+          {
+            testimonies.map(element => (
+              <Testimony 
+                name={element.name}
+                country={element.country}
+                image={element.image}
+                role={element.role}
+                company={element.company}
+                testimony={element.testimony} />
+                ))
+          }
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/testimony.js
+++ src/components/testimony.js
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react'
 import '../css/testimony.css';
 
-function Testimony(props) {
-  return (
-    <div className='container-testimony'>
-      <img className='image-testimony' src={require(`../images/testimony-${props.image}.png`)}
-      alt='Testimony Foto' />
-      <div className='container-text-testimony'>
-        <p className='testimony-name'><strong>{props.name}</strong> en {props.country}</p>
-        <p className='testimony-charge'>{props.role} en <strong>{props.company}</strong></p>
-        <p className='testimony-text'>{props.testimony}</p>
+class Testimony extends Component {;
+  
+  render() {
+    return (
+      <div className='container-testimony'>
+        <img className='image-testimony' src={require(`../images/testimony-${this.props.image}.png`)}
+        alt={`${this.props.name}`} />
+        <div className='container-text-testimony'>
+          <p className='testimony-name'><strong>{this.props.name}</strong> en {this.props.country}</p>
+          <p className='testimony-charge'>{this.props.role} en <strong>{this.props.company}</strong></p>
+          <p className='testimony-text'>{this.props.testimony}</p>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Testimony;
